2dd92a02404f09d26e45a682a610426e6358834f
Harshvardhan Jha <harshvardhanjha4@gmail.com>
2022-09-04 10:30:24 +0530

    Updates

---
+++ precache-manifest.466e660554d2fd18e407dff45d8aadcb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f6dd92c14b6127a32bcbc3e39201e9",
+    "url": "/harshportfolio/index.html"
+  },
+  {
+    "revision": "17c20b8eb62ec4ded09a",
+    "url": "/harshportfolio/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "d4b864b6600d214ca913",
+    "url": "/harshportfolio/static/css/main.e4de2604.chunk.css"
+  },
+  {
+    "revision": "17c20b8eb62ec4ded09a",
+    "url": "/harshportfolio/static/js/2.bce62985.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/harshportfolio/static/js/2.bce62985.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4b864b6600d214ca913",
+    "url": "/harshportfolio/static/js/main.3751b557.chunk.js"
+  },
+  {
+    "revision": "508f35ef700b1e48074c",
+    "url": "/harshportfolio/static/js/runtime-main.47cfacb6.js"
+  },
+  {
+    "revision": "ea556f76415cb789b606039644fff5e6",
+    "url": "/harshportfolio/static/media/Pencil.ea556f76.jpg"
+  },
+  {
+    "revision": "9c55223896f1a99509f79e4f9318ced4",
+    "url": "/harshportfolio/static/media/hello.9c552238.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/harshportfolio/static/media/laptop1.87561258.jpg"
+  },
+  {
+    "revision": "fa7276b6936be889b698c0f45c7bd964",
+    "url": "/harshportfolio/static/media/sam.fa7276b6.jpg"
+  }
+]);
